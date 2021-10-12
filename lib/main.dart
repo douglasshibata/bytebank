7a3e99ed3957d3bc9631f51049fe9db063be4d9a
Douglas Shibata <douglas.shibata11@gmail.com>
2021-10-12 19:00:01 -0300

    adding card and list

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      body:Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text('100.0'),
+          subtitle: Text('10000'),
+        ),
+      ),
       appBar: AppBar(title: Text("Transferências"),),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
